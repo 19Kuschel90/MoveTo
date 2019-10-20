dc6859f3c5abd53cc7221f205af0991382f0f9ad
Heiko Kuschel <H.Kuschel.Rohrbach@gmx.de>
2019-10-20 04:08:06 +0200

    frist data

---
+++ MoveTo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"./conf.xml");
+            XmlNodeList nodeListFolderLink = xDoc.SelectNodes("/links/folderLink");
+            XmlNodeList nodeListMoveTo = xDoc.SelectNodes("/links/moveTo");
+            string folderLink = @"C:\Users\tower\source\repos\MoveTo\MoveTo\bin\Debug\default";
+            string moveTo = @"C:\Users\tower\Pictures\test";
+            // selected folders
+            foreach (XmlNode node in nodeListFolderLink)
+            {
+                Console.WriteLine("Take All");
+                Console.WriteLine(node.InnerText);
+                folderLink = node.InnerText;
+            }
+            foreach (XmlNode node in nodeListMoveTo)
+            {
+                Console.WriteLine("Move To");
+                Console.WriteLine(node.InnerText);
+                moveTo = node.InnerText;
+            }   
+            DirectoryInfo diTop = new DirectoryInfo(folderLink);
+            try
+            {
+                foreach (var fi in diTop.EnumerateFiles())
+                {
+                    string leer = @"\";
+                    System.IO.File.Copy(fi.FullName, moveTo + leer + fi.Name, true);
+                }
+            }
+            catch (UnauthorizedAccessException UnAuthTop)
+            {
+                Console.WriteLine("{0}", UnAuthTop.Message);
+
+            }
+            Console.WriteLine("OK");
+
+            Console.ReadLine();
+        }
+   }
+}
